8a71c801181908dacc118c1984efb1b6d6f61229
Mohamed Tawfik <108617278+MohamedTawfik2021@users.noreply.github.com>
2023-03-18 19:49:16 +0200

    update1

--- Cooler_System/3-APP/main.cpp
+++ Cooler_System/3-APP/main.cpp
@@ -12,33 +12,31 @@
 
 
 #include "DIO.h"
-#include "ADC.h"
 #include "Timer.h"
 
 #include "LCD.h"
 #include "LED.h"
 #include "DC_MOTOR.h"
+#include "LM35.h"
 
 /*Global Variables*/
-  u16 digital_read=0;
-  f32  analog_read=0;
-  f32   temprature=0;
-  
+  u8   temprature=0;
   LCD lcd1;
   u8  fan_duty=50;
   char temp_buffer[7];
   char speed_buffer[7];
+  
 /*Local Functions Prototype*/  
 void Display();
 
 int main(void)
 {
-	 
-	ADC_INPUT temp_sensor(ADC0);
+	LM35 temp_sensor(ADC0);
 	DC_MOTOR fan;
-	fan.Motor_Duty(50);
-	fan.Motor_RUN();
-
+//	fan.Motor_Duty(50);
+//	fan.Motor_RUN();
+	fan.Motor_STOP();
+	
 	LED green_led(PIN_2B);
 	LED yellow_led(PIN_1B);
 	LED red_led(PIN_0B);
@@ -47,16 +45,16 @@ int main(void)
 	
     while (1) 
     {		
-		digital_read = temp_sensor.ADC_ChannelRead();
-		analog_read = digital_read *4.88;
-		temprature  = (analog_read/10);
+
+		temprature = temp_sensor.GetTemperature();
 		
 		if (temprature<20)
 		{
             green_led.LED_ON();
 			yellow_led.LED_OFF();
 			red_led.LED_OFF();	
-			fan_duty =50;
+			fan.Motor_STOP();
+			fan_duty = 0;
 			lcd1.LCD_Clear();
 			Display();		
 			_delay_ms(100);
@@ -67,8 +65,9 @@ int main(void)
 			green_led.LED_ON();
 			yellow_led.LED_ON();
 			red_led.LED_OFF();
-			fan.Motor_Duty(75);
-			fan_duty =75;
+			fan.Motor_Duty(50);
+			fan.Motor_RUN();
+			fan_duty = 50;
 			lcd1.LCD_Clear();
 			Display();		
 			_delay_ms(100);
